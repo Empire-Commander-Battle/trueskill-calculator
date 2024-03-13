b5d90304c0b8476bce81c2c11ba04e7fdbab34da
Maciej Kalandyk <m.kalandyk@outlook.com>
2024-03-13 18:17:16 +0100

    Handle multiple event types

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import trueskill
 import pandas as pd
 
@@ -47,12 +48,11 @@ commander_col_pos = (0, 13)
 victorious_commander_row = 10
 defeated_commander_row = 11
 
-event_type_row = df.iloc[event_type_row_pos[1], event_type_row_pos[0]:]
 rounds_row = df.iloc[rounds_row_pos[1], rounds_row_pos[0]:]
 
 rounds_mask = rounds_row.apply(lambda x: isinstance(x, str) and re.match('R\d', x) is not None)
 rounds_indexes = rounds_row.index[rounds_mask]
-rounds_indexes = [i for i in reversed(rounds_indexes) if event_type_row[i] == 'Internal']
+rounds_indexes = list(reversed(rounds_indexes))
 
 for last_index in range(commander_col_pos[1], df.iloc[:, commander_col_pos[0]].size):
     if isinstance(df.iloc[last_index, commander_col_pos[0]], str):
@@ -61,39 +61,58 @@ for last_index in range(commander_col_pos[1], df.iloc[:, commander_col_pos[0]].s
 
 commanders = df.iloc[commander_col_pos[1]:last_index, commander_col_pos[0]]
 
-ratings_history = []
+ratings_history = {}
 ratings = {}
+
+# helper class because fuck multiple variables
+class Commander:
+    def __init__(self, player_name, get_rating_f):
+        self.name = 'COMMANDER|' + player_name
+        self.player_name = player_name
+        self.rating = get_rating_f(self.name)
 
 print(rounds_indexes)
 for round_no, round_index in enumerate(rounds_indexes):
+    event_type = df.iloc[event_type_row_pos[1], round_index]
+
+    if event_type not in ratings:
+        ratings[event_type] = {}
+
+    def get_rating(name):
+        if name in ratings[event_type]:
+            return ratings[event_type][name]
+
+        return trueskill.Rating()
+
+    def set_rating(name, rating):
+        ratings[event_type][name] = rating
+
     victor_team = []
     looser_team = []
 
     draw = False
+    blacklist = []
 
-    victorious_commander_name = df.iloc[victorious_commander_row, round_index]
-    defeated_commander_name = df.iloc[defeated_commander_row, round_index]
+    print(df.iloc[victorious_commander_row, round_index])
+    print(df.iloc[defeated_commander_row, round_index])
 
-    victorious_commander = 'COMMANDER|' +  victorious_commander_name
-    defeated_commander = 'COMMANDER|' + defeated_commander_name
 
-    victorious_commander_rating = trueskill.Rating()
-    if victorious_commander in ratings:
-        victorious_commander_rating = ratings[victorious_commander]
-    else:
-        ratings[victorious_commander] = victorious_commander_rating
+    victorious_commander = None
+    if isinstance(df.iloc[victorious_commander_row, round_index], str):
+        victorious_commander = Commander(df.iloc[victorious_commander_row, round_index], get_rating)
+        print(victorious_commander.name)
+        blacklist.append(victorious_commander.player_name)
 
-    defeated_commander_rating = trueskill.Rating()
-    if defeated_commander in ratings:
-        defeated_commander_rating = ratings[defeated_commander]
-    else:
-        ratings[defeated_commander] = defeated_commander_rating
+    defeated_commander = None
+    if isinstance(df.iloc[defeated_commander_row, round_index], str):
+        defeated_commander = Commander(df.iloc[defeated_commander_row, round_index], get_rating)
+        print(defeated_commander.name)
+        blacklist.append(defeated_commander.player_name)
 
     for player_index in range(commander_col_pos[1],
                               commander_col_pos[1] + commanders.size):
         player_name = df.iloc[player_index, commander_col_pos[0]]
-        if player_name in (victorious_commander_name,
-                           defeated_commander_name):
+        if player_name in blacklist:
             continue
 
         match df.iloc[player_index, round_index - 1]:
@@ -109,41 +128,57 @@ for round_no, round_index in enumerate(rounds_indexes):
                     case '2':
                         victor_team.append(commanders[player_index])
 
-    victor_team_ratings = [
-        ratings[i] if i in ratings else trueskill.Rating() for i in victor_team]
-    victor_team_ratings.append(victorious_commander_rating)
+    victor_team_ratings = [get_rating(i) for i in victor_team]
 
-    looser_team_ratings = [
-        ratings[i] if i in ratings else trueskill.Rating() for i in looser_team]
-    looser_team_ratings.append(defeated_commander_rating)
+    if victorious_commander is not None:
+        victor_team_ratings.append(victorious_commander.rating)
+
+    looser_team_ratings = [get_rating(i) for i in looser_team]
+
+    if defeated_commander is not None:
+        looser_team_ratings.append(defeated_commander.rating)
 
     victor_team_ratings, looser_team_ratings = trueskill.rate([victor_team_ratings,
                                                                looser_team_ratings],
                                                               [0, 0] if draw else [0, 1])
 
     for player, rating in zip(victor_team, victor_team_ratings):
-        ratings[player] = rating
-    ratings[victorious_commander] = victor_team_ratings[-1]
+        set_rating(player, rating)
+
+    if victorious_commander is not None:
+        set_rating(victorious_commander.name, victor_team_ratings[-1])
 
     for player, rating in zip(looser_team, looser_team_ratings):
-        ratings[player] = rating
-    ratings[defeated_commander] = looser_team_ratings[-1]
+        set_rating(player, rating)
+
+    if defeated_commander is not None:
+        set_rating(defeated_commander.name, looser_team_ratings[-1])
 
     mu_dict = {}
     sigma_dict = {}
 
-    for player, rating in ratings.items():
+    for player, rating in ratings[event_type].items():
         mu_dict[player] = rating.mu
         sigma_dict[player] = rating.sigma
 
-    ratings_history.append((round_no, mu_dict, sigma_dict))
+    if event_type not in ratings_history:
+        ratings_history[event_type] = []
+    ratings_history[event_type].append((round_no, mu_dict, sigma_dict))
 
-# reverse order
-output_dict = {}
-for round_no, mu, sigma in reversed(ratings_history):
-    output_dict[f'mu{round_no}'] = mu
-    output_dict[f'sigma{round_no}'] = sigma
+tables = {}
+for event_type, history in ratings_history.items():
+    output_dict = {}
 
-table = pd.DataFrame.from_dict(output_dict)
+    # reverse order
+    for round_no, mu, sigma in reversed(history):
+        output_dict[f'mu{round_no}'] = mu
+        output_dict[f'sigma{round_no}'] = sigma
 
-table.to_excel(OUTPUT_FILENAME)
+    table = pd.DataFrame.from_dict(output_dict)
+    table.sort_values(f'mu{history[-1][0]}', inplace=True, ascending=False)
+
+    tables[event_type] = table
+
+with pd.ExcelWriter(OUTPUT_FILENAME) as writer:
+    for event_type, table in tables.items():
+        table.to_excel(writer, sheet_name=event_type)
